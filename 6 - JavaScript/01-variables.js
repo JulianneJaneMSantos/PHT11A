27a3f3b233fb09306202688b2ed25513729f3b54
JulianneJaneMSantos <julianne.jane.s@gmail.com>
2024-08-13 13:38:21 +0800

    Previous studies I have

---
+++ 6 - JavaScript/01-variables.js
@@ -0,0 +1,20 @@
+console.log("Hello World!");
+
+// Variables - store values (container)
+// let, const, var (var is not beginner friendly, does the same thing as let)
+
+// let - mutable/editable value
+let firstName = "Jared";
+console.log("Firstname:", firstName);
+
+firstName = "Ariston";
+console.log("Firstname:", firstName);
+
+// const - immutable/uneditable value
+const lastName = "Hortaleza";
+console.log("Lastname:", lastName);
+
+// This gives us an error!
+// lastName = "Catipay";
+// console.log("Lastname:", lastName);
+
---
+++ 6 - JavaScript/02-datatypes.js
@@ -0,0 +1,26 @@
+// Data Types - Primitive Types (Dont complicate things, for singular value)
+
+// Number: Represent numeric values (35, 3.14)
+let age = -22.3;
+console.log("Age: " , age);
+console.log("Typeof age:", typeof age);
+
+//strings: Represent textual values ("abc", 'def123')
+let name = "John";
+console.log("Name: " , name);
+console.log("Typeof name:", typeof name);
+
+//Boolean: Either true or false
+let isAdmin = true;
+console.log("isAdmin: " , isAdmin);
+console.log("Typeof isAdmin:", typeof isAdmin);
+
+// Null: Represent INTENTIONAl absence of a value
+let car = null;
+console.log("Car: " , car);
+console.log("Typeof car:", typeof car);
+
+// Undefined: Representing UNINTENTIONAL absent of value
+let city;
+console.log("city?" , city);
+console.log("Typeof city:", typeof city);
